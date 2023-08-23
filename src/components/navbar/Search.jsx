9d875aeb032b1711ba7f8180416b0bbae1f6b22f
osman goni <osman.ice.32@gmail.com>
2023-08-23 08:43:29 +0600

    projectstarting

---
+++ src/components/navbar/Search.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <>
+      <form>
+        <input
+          className="outline-none border-none mr-2"
+          type="search"
+          name="search"
+          placeholder="Search"
+        />
+      </form>
+    </>
+  );
+}
