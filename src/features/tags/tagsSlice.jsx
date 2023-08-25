ccbccd587e361575174f4178d5b1dcd4ae75b8b9
osman goni <osman.ice.32@gmail.com>
2023-08-25 18:44:03 +0600

    tags added

---
+++ src/features/tags/tagsSlice.jsx
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import { getTags } from "./tagsAPI";
+
+const initialState = {
+  tags: [],
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+//asyncThunk
+export const fetchTags = createAsyncThunk("tags/fetchtags", async () => {
+  const tags = await getTags();
+  return tags;
+});
+
+const tagSlice = createSlice({
+  name: "tags",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTags.pending, (state) => {
+        state.isError = false;
+        state.isLoading = true;
+      })
+      .addCase(fetchTags.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.tags = action.payload;
+      })
+      .addCase(fetchTags.rejected, (state, action) => {
+        state.isLoading = false;
+        state.tags = [];
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+export default tagSlice.reducer;
