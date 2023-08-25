c9ece22ebd00c1cbff7773c9de2e4af3f5fbaf3e
osman goni <osman.ice.32@gmail.com>
2023-08-25 12:55:30 +0600

    videos feature completed

--- src/components/grid/VideoGridItem.jsx
+++ src/components/grid/VideoGridItem.jsx
@@ -1,47 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function VideoGridItem() {
+export default function VideoGridItem({ video = {} }) {
+  const { id, thumbnail, title, duration, author, avatar, views, date } = video;
   return (
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to="/Videos/1">
-            <img
-              src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
-              className="w-full h-auto"
-              alt="Some video title"
-            />
+          <Link to={`Videos/${id}`}>
+            <img src={thumbnail} className="w-full h-auto" alt={title} />
           </Link>
 
           <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-            12:10
+            {duration}
           </p>
         </div>
 
         <div className="flex flex-row mt-2 gap-2">
-          <Link to="/Videos/1" className="shrink-0">
-            <img
-              src="https://avatars.githubusercontent.com/u/73503432?v=4"
-              className="rounded-full h-6 w-6"
-              alt="Learn with Sumit"
-            />
+          <Link to={`Videos/${id}`} className="shrink-0">
+            <img src={avatar} className="rounded-full h-6 w-6" alt={author} />
           </Link>
 
           <div clas="flex flex-col">
-            <Link to="/Videos/1">
-              <p className="text-slate-900 text-sm font-semibold">
-                Video title
-              </p>
+            <Link to={`Videos/${id}`}>
+              <p className="text-slate-900 text-sm font-semibold">{title}</p>
             </Link>
             <Link
               className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-              to="#"
+              to={`Videos/${id}`}
             >
-              Learn with Sumit
+              {author}
             </Link>
             <p className="text-gray-400 text-xs mt-1">
-              200 views . May 3, 2022
+              {views}. {date}
             </p>
           </div>
         </div>
