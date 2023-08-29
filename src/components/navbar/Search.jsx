71abfd2389dadb6e4264542e421211fdcceaa112
osman goni <osman.ice.32@gmail.com>
2023-08-30 00:09:40 +0600

    filter section half

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tagSearched } from "../../features/filter/filterSlice";
 
 export default function Search() {
+  const { search } = useSelector((state) => state.filter);
+  const [input, setInput] = useState(search);
+  const dispatch = useDispatch();
+  const handleSearch = (e) => setInput(e.target.value);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(tagSearched(input));
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <input
           className="outline-none border-none mr-2"
           type="search"
           name="search"
           placeholder="Search"
+          value={input}
+          onChange={handleSearch}
         />
       </form>
     </>
--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tagRemoved, tagSelected } from "../../features/filter/filterSlice";
 
 export default function Tag({ title }) {
+  const dispatch = useDispatch();
+  const { tags } = useSelector((state) => state.filter);
+  const isSelected = tags.includes(title) ? true : false;
+  //console.log(isSelected);
+  const handleSelected = () => {
+    if (isSelected) {
+      dispatch(tagRemoved(title));
+    }
+    if (!isSelected) {
+      dispatch(tagSelected(title));
+    }
+  };
+  const style = isSelected
+    ? "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+    : "bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer";
   return (
-    <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer">
+    <div className={style} onClick={handleSelected}>
       {title}
     </div>
   );
