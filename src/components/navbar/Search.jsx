efe31b186ed749203c3aae1f3e89edbd78dde402
osman goni <osman.ice.32@gmail.com>
2023-08-30 16:59:28 +0600

    filter section added

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useMatch, useNavigate } from "react-router-dom";
 import { tagSearched } from "../../features/filter/filterSlice";
 
 export default function Search() {
   const { search } = useSelector((state) => state.filter);
   const [input, setInput] = useState(search);
   const dispatch = useDispatch();
+  const match = useMatch("/");
+  const navigate = useNavigate();
   const handleSearch = (e) => setInput(e.target.value);
 
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(tagSearched(input));
+    //
+    if (!match) {
+      navigate("/");
+    }
   };
 
   return (
